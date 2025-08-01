72ea29fd045aee3b614568e7a88e0e830a6515f9
Bharat <bharat.codewolf@gmail.com>
2025-08-02 00:48:45 +0530

    fix: resolve deployment issues - add crypto-js for QR utils and fix user routes import

--- routes/api.ts
+++ routes/api.ts
@@ -22,7 +22,7 @@ import departmentsRoutes from "./departments";
 import gatesRoutes from "./gates";
 import userDepartmentMapRoutes from "./user_department_map";
 import qrRoutes from "./qr";
-const userRoutes = require("./user");
+const userRoutes = require("./user.js");
 
 // User Notifications Endpoints
 
